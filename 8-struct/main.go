d8a4e90dbd01926ad4cd6b0c1ba77ade7997bc4c
JPauloMoura <joaopaulo.034a@gmail.com>
2021-04-08 22:58:12 -0300

    trabalhando com structs

---
+++ 8-struct/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type Pessoa struct {
+	Nome         string
+	Idade        int
+	TemGraduacao bool
+	Endereco     Endereco
+}
+
+type Endereco struct {
+	Cidade string
+	Estado string
+	Rua    string
+	Numero int
+}
+
+func main() {
+	// declaração 1
+	var jose Pessoa = Pessoa{}
+	jose.Nome = "José da Silva"
+	jose.Idade = 18
+	jose.Endereco.Rua = "Violeta"
+	//observe que os valores não declarados receberam um valor padrão
+	fmt.Println(jose) //{José da Silva 18 false {  Violeta 0}}
+
+	// declaração 2
+	carlos := Pessoa{}
+	carlos.Nome = "Carlos da Silva"
+	carlos.Idade = 18
+	carlos.Endereco.Rua = "Violeta"
+	fmt.Println(carlos)
+
+	// declaração 3
+	bruna := Pessoa{
+		Nome: "Bruna da Silva",
+		Endereco: Endereco{
+			Numero: 786,
+		},
+	}
+	fmt.Println(bruna)
+
+	// declaração 4
+	magali := Pessoa{"Magali da Silva", 18, true, Endereco{"Altos", "SP", "rua 2", 909}}
+	fmt.Println(magali)
+
+}
---
+++ 8-struct/sobre.md
@@ -0,0 +1,18 @@
+1) O go quando queremos denotar um ponteiro nulo usamos o `nil`
+ex: 
+```go
+  nome := nil //errado
+  var nome string = nil //errado
+  var nome *string = nil //certo
+```
+
+2) struct é bem parecida com uma classe em POO. Crie uma struct com as seguites
+informaçoes:
+- nome
+- idade
+- temGraduacao
+- endereco
+  - cidade
+  - estado
+  - rua
+  - numero
