6168c77430e3099ef2a319f84acbd0ffc7d8c787
JPaulo-Moura <joaopaulo.034a@gmail.com>
2022-09-11 23:30:19 -0300

    feat: :sparkles: utilizando intefaces

--- 11-Interfaces/main.go
+++ 11-Interfaces/main.go
@@ -2,14 +2,33 @@ package main
 
 import "fmt"
 
-type FuncionarioIT interface {
+// 1) uma interface é como um 'contrato' e define os metodos que uma struct deve possuir para ser consideranda desse tipo
+// ex: se temos a interface A com os metodos b1 e b2, todo struct que implementar os metodos b1 e b2 será do tipo A
+
+// 2) Uma interface é a definição de um conjunto de métodos comuns a um ou mais tipos. É o que permite a criação de tipos polimórficos em Go.
+
+// 3) Com a ajuda de interface podemos criar funçoes que recebem como argumento um tipo de interface, e todos as struct
+// que são do tipo dessa interface podem ser usadas.
+
+// 4) funções que recebem uma interface podem ter comportamentos diferentes dependendo do seu tipo original
+func main() {
+	f := Funcionario{Nome: "Carlos", ativo: false}
+	adm := Adm{Funcionario: Funcionario{Nome: "jp", ativo: true}, Permisao: "tudo"}
+	f.Info()
+	trocarStatusAtivo(&adm)
+}
+
+func trocarStatusAtivo(f IFuncionario) {
+	f.setAtivo()
+}
+
+type IFuncionario interface {
 	Info()
 	setAtivo()
 }
 
 type Funcionario struct {
 	Nome  string
-	Email string
 	ativo bool
 }
 
@@ -26,7 +45,6 @@ type Cliente struct {
 
 func (f Funcionario) Info() {
 	fmt.Println("Nome:", f.Nome)
-	fmt.Println("Email:", f.Email)
 	fmt.Println("Ativo:", f.ativo)
 }
 
@@ -39,16 +57,4 @@ func (c Cliente) Info() {
 	fmt.Println("Cliente:", c.Nome)
 	fmt.Println("Email:", c.Email)
 	fmt.Println("Ativo:", c.ativo)
-}
-
-func trocarStatusAtivo(f FuncionarioIT) {
-	f.setAtivo()
-}
-
-func main() {
-	f := Funcionario{Nome: "Carlos", Email: "c@gmail", ativo: false}
-	adm := Adm{Funcionario: Funcionario{Nome: "jp", Email: "j@gmail", ativo: true}, Permisao: "tudo"}
-	f.Info()
-	trocarStatusAtivo(&adm)
-
 }
