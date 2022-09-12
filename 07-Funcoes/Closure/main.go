678d8114f29ce1e6d26fbd6f84c70711e7ff15c3
JPaulo-Moura <joaopaulo.034a@gmail.com>
2022-09-11 23:12:57 -0300

    feat:

---
+++ 07-Funcoes/Closure/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+/*
+	Funções Closure, são funções que "lembram" do escopo de onde foram declaradas.
+
+*/
+
+func main() {
+	listaDeNomes := []string{"jp", "vick", "billie", "rafael", "gustavo", "maria"}
+	proximoDalista := PercorrerLista(listaDeNomes)
+
+	fmt.Println(proximoDalista())
+	fmt.Println(proximoDalista())
+	fmt.Println(proximoDalista())
+	fmt.Println(proximoDalista())
+	fmt.Println(proximoDalista())
+	fmt.Println(proximoDalista())
+}
+
+type ProximoItem func() string
+
+// PercorrerLista devolve a função ProximoItem, que vai ter na sua "memória" o calor te todoas a veriaveis
+// que foram declaradas no escopo em que ela foi criada.
+// ou seja, a cada nova chamada da função ProximoItem que foi retornada, ela vai lembrar de todos os seu valores anteriores.
+func PercorrerLista(lista []string) ProximoItem {
+	var index int
+	var item string
+
+	funcBuscarItem := func() string {
+		if index < len(lista) {
+			item = lista[index]
+			index++
+		}
+		return item
+	}
+
+	return funcBuscarItem
+}
